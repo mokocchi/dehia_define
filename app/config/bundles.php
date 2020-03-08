3de4756731615462d3f6bd30cb5ff9fcd6aa47b8
Jose Arcidiacono <josefine2_94@hotmail.com>
2020-03-08 16:51:26 -0300

    Agregar más dependencias

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -13,4 +13,6 @@ return [
     WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
