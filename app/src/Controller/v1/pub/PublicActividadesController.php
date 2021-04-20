730b45bc04f70f489f2dee45f5c766acc17e86b9
Jose Arcidiacono <j2arcidiacono@outlook.com>
2021-04-20 09:42:59 -0300

    sanitize filenames

--- app/src/Controller/v1/pub/PublicActividadesController.php
+++ app/src/Controller/v1/pub/PublicActividadesController.php
@@ -263,9 +263,30 @@ class PublicActividadesController extends BaseController
         $fileContent = json_encode($JSON, JSON_PRETTY_PRINT);
         $response = new Response($fileContent);
 
+
+        # https://stackoverflow.com/a/42058764
+        function filter_filename($filename, $beautify=true) {
+            // sanitize filename
+            $filename = preg_replace(
+                '~
+                [<>:"/\\|?*]|            # file system reserved https://en.wikipedia.org/wiki/Filename#Reserved_characters_and_words
+                [\x00-\x1F]|             # control characters http://msdn.microsoft.com/en-us/library/windows/desktop/aa365247%28v=vs.85%29.aspx
+                [\x7F\xA0\xAD]|          # non-printing characters DEL, NO-BREAK SPACE, SOFT HYPHEN
+                [#\[\]@!$&\'()+,;=]|     # URI reserved https://tools.ietf.org/html/rfc3986#section-2.2
+                [{}^\~`]                 # URL unsafe characters https://www.ietf.org/rfc/rfc1738.txt
+                ~x',
+                '-', $filename);
+            // avoids ".", ".." or ".hiddenFiles"
+            $filename = ltrim($filename, '.-');
+            // maximize filename length to 255 bytes http://serverfault.com/a/9548/44086
+            $ext = pathinfo($filename, PATHINFO_EXTENSION);
+            $filename = mb_strcut(pathinfo($filename, PATHINFO_FILENAME), 0, 255 - ($ext ? strlen($ext) + 1 : 0), mb_detect_encoding($filename)) . ($ext ? '.' . $ext : '');
+            return $filename;
+        }
+
         $disposition = HeaderUtils::makeDisposition(
             HeaderUtils::DISPOSITION_ATTACHMENT,
-            iconv("UTF-8", "ASCII//TRANSLIT", $actividad->getNombre()) . '.json'
+                    filter_filename($actividad->getNombre() . '.json')
         );
 
         $response->headers->set('Content-Disposition', $disposition);
